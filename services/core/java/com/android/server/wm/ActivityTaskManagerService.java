8475b1b2feb76b390168be6f1bcca09ee3ec1ab0
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-19 20:46:32 -0300

    Squashed Import of PixelPropsUtils
    * Imported list from our 13 base [1] [2]
    * Removed pixel 6 pro spoof
    * Clean-ups and update logic with PropImitationHooks
    * Simplify and improve bypassing for PPU `onTaskStackChanged` listening
    * Allow to pass strong integrity by building rro
    
    [1] https://github.com/PixysOS/frameworks_base/commits/cb5e356ab56d1edea014a3f93ca36a9f2004a2bf/core/java/com/android/internal/util/pixys/PixelPropsUtils.java
    [2] https://github.com/TheParasiteProject/frameworks_base/blob/432636254ebad0fd5e78c624fccfca9a40c6a0cf/core/java/com/android/internal/util/custom/PixelPropsUtils.java
    
    Co-authored-by: johnmart19 <johnivan19999@gmail.com>
    Co-authored-by: aswin7469 <aswinas@pixysos.com>
    Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
    Co-authored-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Co-authored-by: Danny Lin <danny@kdrag0n.dev>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Dyneteve <dyneteve@hentaios.com>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: Chris Crump <chriscrawford893@gmail.com>
    Co-authored-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- services/core/java/com/android/server/wm/ActivityTaskManagerService.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -287,6 +287,7 @@ import com.android.wm.shell.Flags;
 import com.android.server.usage.AppStandbyInternal;
 
 import com.android.internal.util.everest.cutout.CutoutFullscreenController;
+import com.android.internal.util.everest.PixelPropsUtils;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -1991,7 +1992,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     @Override
     public RootTaskInfo getFocusedRootTaskInfo() throws RemoteException {
-        enforceTaskPermission("getFocusedRootTaskInfo()");
+        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
+            enforceTaskPermission("getFocusedRootTaskInfo()");
+        }
         final long ident = Binder.clearCallingIdentity();
         try {
             synchronized (mGlobalLock) {
@@ -3110,7 +3113,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
     /** Sets the task stack listener that gets callbacks when a task stack changes. */
     @Override
     public void registerTaskStackListener(ITaskStackListener listener) {
-        enforceTaskPermission("registerTaskStackListener()");
+        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
+            enforceTaskPermission("registerTaskStackListener()");
+        }
         mTaskChangeNotificationController.registerTaskStackListener(listener);
     }
 
