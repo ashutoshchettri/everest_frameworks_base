4e86adfa9bf17aaafa585d9b2cbee139f2ac4b2a
SamarV-121 <samarvispute121@pm.me>
2023-04-23 13:44:56 +0530

    SystemUI: Add brightness slider styles [1/3]
    Change-Id: I0f2c05a3d0b83ba9ddaca23e08e97e87a5c69a2c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13268,6 +13268,12 @@ public final class Settings {
                 = "pocket_mode_enabled";
 
         /**
+         * Brightness slider styles
+         * @hide
+         */
+        public static final String BRIGHTNESS_SLIDER_STYLE = "brightness_slider_style";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
